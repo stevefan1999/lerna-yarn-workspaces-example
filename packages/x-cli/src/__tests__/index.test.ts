75bb9910f47016e397aea1cd422c056c686a2cad
Steve Fan <29133953+stevefan1999@users.noreply.github.com>
2018-08-16 16:22:08 +0800

    Update test

--- packages/x-cli/src/__tests__/index.test.ts
+++ packages/x-cli/src/__tests__/index.test.ts
@@ -2,7 +2,7 @@ import '@wordpress/jest-console'
 import { cli } from '@foo/x-cli'
 
 describe('x-cli', () => {
-  test('should log', async () => {
+  test('should run fine', async () => {
     expect(await cli()).toBe(true)
     expect(console).toHaveLoggedWith('Hello')
   })
