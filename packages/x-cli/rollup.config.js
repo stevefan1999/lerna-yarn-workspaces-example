04c855f1a558fd0d6edca5122eacc6057b27b6e9
Steve Fan <29133953+stevefan1999@users.noreply.github.com>
2018-08-16 16:31:21 +0800

    changed package fields

--- packages/x-cli/rollup.config.js
+++ packages/x-cli/rollup.config.js
@@ -1,11 +1,12 @@
 import base from '../../rollup.config'
+
 const pkg = require('./package.json')
 
 export default {
   ...base,
   input: 'src/main.ts',
   output: [
-    { file: pkg.cjs, format: 'cjs' },
+    { file: pkg.main, format: 'cjs' },
     { file: pkg.module, format: 'esm' }
   ]
 }
