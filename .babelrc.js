018754a432f0fa2424d0659b6ebce70919657025
Steve Fan <29133953+stevefan1999@users.noreply.github.com>
2018-08-18 16:04:34 +0800

    Update babel to use stage 2 presets

--- .babelrc.js
+++ .babelrc.js
@@ -5,7 +5,16 @@ module.exports = {
       {
         modules: false,
         useBuiltIns: true
-      }
+      },
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      '@babel/plugin-proposal-function-sent',
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-throw-expressions',
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-syntax-import-meta',
+      ['@babel/plugin-proposal-class-properties', { loose: false }],
+      '@babel/plugin-proposal-json-strings'
     ]
   ]
 }
