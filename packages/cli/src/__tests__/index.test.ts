ce51b732e548d4fe3ee4e59ddb7858f77baed049
Steve Fan <29133953+stevefan1999@users.noreply.github.com>
2018-08-18 19:14:36 +0800

    Added spy stubbing

--- packages/cli/src/__tests__/index.test.ts
+++ packages/cli/src/__tests__/index.test.ts
@@ -1,11 +1,24 @@
-import { AsyncTest, Expect, SpyOn, TestFixture } from 'alsatian'
+import {
+  AsyncTest, Expect, RestorableFunctionSpy, SpyOn, Teardown, TestFixture
+} from 'alsatian' // prettier-ignore
 import { cli } from '@foo/cli'
 
 @TestFixture('CLI test')
 export class IndexTest {
+  private spies: Array<RestorableFunctionSpy> = []
+
+  @Teardown
+  teardown(): void {
+    this.spies.map(_ => _.restore())
+  }
+
+  addSpies(...args: RestorableFunctionSpy[]): void {
+    ;(this.spies = args).map(_ => _.andStub())
+  }
+
   @AsyncTest()
   async 'should run fine'(): Promise<void> {
-    SpyOn(console, 'log')
+    this.addSpies(SpyOn(console, 'log'))
     Expect(await cli()).toBe(true)
     Expect(console.log).toHaveBeenCalledWith('Hello')
   }
--- packages/core/src/__tests__/index.test.ts
+++ packages/core/src/__tests__/index.test.ts
@@ -1,11 +1,24 @@
-import { Expect, SpyOn, Test, TestFixture } from 'alsatian'
+import {
+  Expect, RestorableFunctionSpy, SpyOn, Teardown, Test, TestFixture
+} from 'alsatian' // prettier-ignore
 import { awesomeFn } from '@foo/core'
 
 @TestFixture('test core')
 export class IndexTest {
+  private spies: Array<RestorableFunctionSpy> = []
+
+  @Teardown
+  teardown(): void {
+    this.spies.map(_ => _.restore())
+  }
+
+  addSpies(...args: RestorableFunctionSpy[]): void {
+    ;(this.spies = args).map(_ => _.andStub())
+  }
+
   @Test()
   'should log'(): void {
-    SpyOn(console, 'log')
+    this.addSpies(SpyOn(console, 'log'))
     awesomeFn()
     Expect(console.log).toHaveBeenCalledWith('Hello')
   }
